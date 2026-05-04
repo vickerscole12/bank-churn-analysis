31195078072443f8b2a8bcaa2cceb9df7dd9cb74
vickerscole12 <114261063+vickerscole12@users.noreply.github.com>
2026-05-04 17:05:22 -0400

    Delete WebsiteDemo/dashboard-app directory

--- WebsiteDemo/dashboard-app/src/app.js
+++
@@ -1,7 +0,0 @@
-/* const app = () => {
-    // This function can be used to initialize any dynamic content or interactivity in the future
-    console.log("Dashboard App Initialized");
-};
-
-// Call the app function to initialize the application
-app();*/
